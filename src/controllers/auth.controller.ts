b43fa219bf7f4d2e24090dd2dcb19f66870d92e1
TheDarkWolf <franciscopugh01@gmail.com>
2019-10-24 16:28:15 -0300

    Create the token

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import User, { IUser } from '../models/User';
+import jwt from 'jsonwebtoken';
 
 export const signup = async (req: Request, res: Response) => {
     // Saving a new User
@@ -8,9 +9,14 @@ export const signup = async (req: Request, res: Response) => {
         email: req.body.email,
         password: req.body.password
     });
+    user.password = await user.encryptPassword(user.password); //Encripto la contraseña original y la misma es devuelta
     const savedUser = await user.save();
 
-    res.send('signup');
+    // Token
+    const token:string = jwt.sign({_id: savedUser._id}, process.env.TOKEN_SECRET || 'token_secret');
+
+    res.header('auth_token', token).json(savedUser);
+
 };
 
 export const signin = (req: Request, res: Response) => {
